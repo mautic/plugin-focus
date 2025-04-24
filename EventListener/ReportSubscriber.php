f05a0b5ce4dadf6c77bd0269755c710ac6603229
Alan Wierzchoń <alan.wierzchon@comarch.pl>
2025-04-24 17:26:06 +0200

    feat: [DPMMA-3016] report with focus item leads stat datasource

--- EventListener/ReportSubscriber.php
+++ EventListener/ReportSubscriber.php
@@ -12,12 +12,14 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 final class ReportSubscriber implements EventSubscriberInterface
 {
     public const CONTEXT_FOCUS_STATS = 'focus_stats';
+    public const CONTEXT_FOCUS_LEADS = 'focus_leads';
     public const FOCUS_GROUP         = 'focus';
     public const PREFIX_FOCUS        = 'f';
     public const PREFIX_STATS        = 'fs';
     public const PREFIX_REDIRECTS    = 'r';
     public const PREFIX_TRACKABLES   = 't';
     public const PREFIX_CATEGORIES   = 'c';
+    public const PREFIX_LEADS        = 'l';
 
     public static function getSubscribedEvents(): array
     {
@@ -32,7 +34,7 @@ final class ReportSubscriber implements EventSubscriberInterface
      */
     public function onReportBuilder(ReportBuilderEvent $event): void
     {
-        if (!$event->checkContext(self::CONTEXT_FOCUS_STATS)) {
+        if (!$event->checkContext([self::CONTEXT_FOCUS_LEADS, self::CONTEXT_FOCUS_STATS])) {
             return;
         }
 
@@ -43,20 +45,20 @@ final class ReportSubscriber implements EventSubscriberInterface
                 'alias'   => 'focus_id',
                 'formula' => 'MAX('.self::PREFIX_FOCUS.'.id)',
             ],
-            self::PREFIX_FOCUS.'.category' => [
-                'label'   => 'mautic.core.category',
-                'type'    => 'html',
-                'alias'   => 'category_name',
-                'formula' => 'MAX('.self::PREFIX_CATEGORIES.'.title)',
-            ],
             self::PREFIX_FOCUS.'.name' => [
-                'label'   => 'mautic.core.name',
+                'label'   => 'mautic.report.focus.name',
                 'type'    => 'html',
                 'alias'   => 'focus_name',
                 'formula' => 'MAX('.self::PREFIX_FOCUS.'.name)',
             ],
+            self::PREFIX_FOCUS.'.category' => [
+                'label'   => 'mautic.report.focus.category',
+                'type'    => 'html',
+                'alias'   => 'category_name',
+                'formula' => 'MAX('.self::PREFIX_CATEGORIES.'.title)',
+            ],
             self::PREFIX_FOCUS.'.description' => [
-                'label'   => 'mautic.core.description',
+                'label'   => 'mautic.report.focus.description',
                 'type'    => 'html',
                 'alias'   => 'focus_desc',
                 'formula' => 'MAX('.self::PREFIX_FOCUS.'.description)',
@@ -68,7 +70,7 @@ final class ReportSubscriber implements EventSubscriberInterface
                 'formula' => 'MAX('.self::PREFIX_FOCUS.'.focus_type)',
             ],
             self::PREFIX_FOCUS.'.style' => [
-                'label'   => 'mautic.focus.tab.focus_style',
+                'label'   => 'mautic.report.focus.style',
                 'type'    => 'html',
                 'alias'   => 'focus_style',
                 'formula' => 'MAX('.self::PREFIX_FOCUS.'.style)',
@@ -186,17 +188,153 @@ final class ReportSubscriber implements EventSubscriberInterface
             'display_name' => 'mautic.focus.graph.stats',
             'columns'      => $columns,
         ];
-        $context = self::CONTEXT_FOCUS_STATS;
 
-        $event->addTable($context, $data, self::FOCUS_GROUP);
+        $event->addTable(self::CONTEXT_FOCUS_STATS, $data, self::FOCUS_GROUP);
+        $this->addFocusLeadsTable($event, $columns);
     }
 
+    private function addFocusLeadsTable(&$event, $columns): void
+    {
+        $columnsLeads = [
+            self::PREFIX_LEADS.'.id' => [
+                'label'   => 'mautic.report.field.lead.id',
+                'type'    => 'html',
+                'alias'   => 'lead_id',
+                'formula' => 'MAX('.self::PREFIX_LEADS.'.id)',
+            ],
+            self::PREFIX_LEADS.'.firstname' => [
+                'label'   => 'mautic.report.field.lead.firstname',
+                'type'    => 'html',
+                'alias'   => 'lead_firstname',
+                'formula' => 'MAX('.self::PREFIX_LEADS.'.firstname)',
+            ],
+            self::PREFIX_LEADS.'.lastname' => [
+                'label'   => 'mautic.report.field.lead.lastname',
+                'type'    => 'html',
+                'alias'   => 'lead_lastname',
+                'formula' => 'MAX('.self::PREFIX_LEADS.'.lastname)',
+            ],
+            self::PREFIX_LEADS.'.email' => [
+                'label'   => 'mautic.report.field.lead.email',
+                'type'    => 'html',
+                'alias'   => 'lead_email',
+                'formula' => 'MAX('.self::PREFIX_LEADS.'.email)',
+            ],
+            self::PREFIX_LEADS.'.company' => [
+                'label'   => 'mautic.report.field.lead.company',
+                'type'    => 'html',
+                'alias'   => 'lead_firstname',
+                'formula' => 'MAX('.self::PREFIX_LEADS.'.company)',
+            ],
+            self::PREFIX_TRACKABLES.'.hits' => [
+                'label'   => 'mautic.report.focus.hits',
+                'type'    => 'html',
+                'alias'   => 'hit_count',
+                'formula' => 'CASE 
+                    WHEN '.self::PREFIX_STATS.'.type = "view" THEN (
+                        SELECT COUNT(fs2.id) 
+                        FROM '.MAUTIC_TABLE_PREFIX.'focus_stats fs2 
+                        INNER JOIN '.MAUTIC_TABLE_PREFIX.'focus f2 
+                        ON f2.id = fs2.focus_id 
+                        WHERE fs2.type = "view" 
+                        AND f2.id = '.self::PREFIX_FOCUS.'.id
+                        AND fs2.lead_id = '.self::PREFIX_LEADS.'.id
+                        GROUP BY f2.id
+                    )
+                    WHEN '.self::PREFIX_STATS.'.type = "submission" THEN (
+                        SELECT COUNT(fs2.id) 
+                        FROM '.MAUTIC_TABLE_PREFIX.'focus_stats fs2 
+                        INNER JOIN '.MAUTIC_TABLE_PREFIX.'focus f2 
+                        ON f2.id = fs2.focus_id 
+                        WHERE fs2.type = "submission" 
+                        AND f2.id = '.self::PREFIX_FOCUS.'.id
+                        AND fs2.lead_id = '.self::PREFIX_LEADS.'.id
+                        GROUP BY f2.id
+                    )
+                    WHEN '.self::PREFIX_STATS.'.type = "click" THEN (
+                        SELECT COUNT(fs2.id) 
+                        FROM '.MAUTIC_TABLE_PREFIX.'focus_stats fs2 
+                        INNER JOIN '.MAUTIC_TABLE_PREFIX.'focus f2 
+                        ON f2.id = fs2.focus_id 
+                        WHERE fs2.type = "click" 
+                        AND f2.id = '.self::PREFIX_FOCUS.'.id
+                        AND fs2.lead_id = '.self::PREFIX_LEADS.'.id
+                        GROUP BY f2.id
+                    )
+                END',
+            ],
+            self::PREFIX_STATS.'.conversion_rate_click' => [
+                'label'   => 'mautic.report.focus.ratio.click',
+                'type'    => 'html',
+                'alias'   => 'conversion_rate_click',
+                'formula' => 'CASE
+                    WHEN '.self::PREFIX_STATS.'.type = "click" THEN (
+                        SELECT
+                            ROUND(
+                                (
+                                    SELECT COUNT(fs2.id)
+                                    FROM '.MAUTIC_TABLE_PREFIX.'focus_stats fs2
+                                    WHERE fs2.type = "click"
+                                    AND fs2.focus_id = '.self::PREFIX_STATS.'.focus_id
+                                    AND fs2.lead_id = '.self::PREFIX_LEADS.'.id
+                                ) * 100.0 /
+                                NULLIF((
+                                    SELECT COUNT(fs3.id)
+                                    FROM '.MAUTIC_TABLE_PREFIX.'focus_stats fs3
+                                    WHERE fs3.type = "view"
+                                    AND fs3.focus_id = '.self::PREFIX_STATS.'.focus_id
+                                    AND fs3.lead_id = '.self::PREFIX_LEADS.'.id
+                                ), 0)
+                            , 2)
+                    )
+                    ELSE NULL
+                END',
+            ],
+            self::PREFIX_STATS.'.conversion_rate_submission' => [
+                'label'   => 'mautic.report.focus.ratio.submission',
+                'type'    => 'html',
+                'alias'   => 'conversion_rate_submission',
+                'formula' => 'CASE
+                    WHEN '.self::PREFIX_STATS.'.type = "submission" THEN (
+                        SELECT
+                            ROUND(
+                                (
+                                    SELECT COUNT(fs2.id)
+                                    FROM '.MAUTIC_TABLE_PREFIX.'focus_stats fs2
+                                    WHERE fs2.type = "submission"
+                                    AND fs2.focus_id = '.self::PREFIX_STATS.'.focus_id
+                                    AND fs2.lead_id = '.self::PREFIX_LEADS.'.id
+                                ) * 100.0 /
+                                NULLIF((
+                                    SELECT COUNT(fs3.id)
+                                    FROM '.MAUTIC_TABLE_PREFIX.'focus_stats fs3
+                                    WHERE fs3.type = "view"
+                                    AND fs3.focus_id = '.self::PREFIX_STATS.'.focus_id
+                                    AND fs3.lead_id = '.self::PREFIX_LEADS.'.id
+                                ), 0)
+                            , 2)
+                    )
+                    ELSE NULL
+                END',
+            ],
+        ];
+
+        // all row lead records are unique
+        unset($columns[self::PREFIX_TRACKABLES.'.unique_hits']);
+
+        $data = [
+            'display_name' => 'mautic.report.datasource.focus.leads',
+            'columns'      => array_merge($columns, $columnsLeads),
+        ];
+
+        $event->addTable(self::CONTEXT_FOCUS_LEADS, $data, self::FOCUS_GROUP);
+    }
     /**
      * Initialize the QueryBuilder object to generate reports from.
      */
     public function onReportGenerate(ReportGeneratorEvent $event): void
     {
-        if (!$event->checkContext([self::CONTEXT_FOCUS_STATS])) {
+        if (!$event->checkContext([self::CONTEXT_FOCUS_STATS, self::CONTEXT_FOCUS_LEADS])) {
             return;
         }
 
@@ -208,13 +346,30 @@ final class ReportSubscriber implements EventSubscriberInterface
                 self::PREFIX_TRACKABLES.'.channel_id = '.self::PREFIX_STATS.'.focus_id AND '.
                 self::PREFIX_TRACKABLES.'.channel = "focus"')
             ->leftJoin(self::PREFIX_STATS, MAUTIC_TABLE_PREFIX.'page_redirects', self::PREFIX_REDIRECTS,
-                self::PREFIX_REDIRECTS.'.id = '.self::PREFIX_TRACKABLES.'.redirect_id')
-            ->groupBy(self::PREFIX_STATS.'.focus_id', self::PREFIX_STATS.'.type');
+                self::PREFIX_REDIRECTS.'.id = '.self::PREFIX_TRACKABLES.'.redirect_id');
 
         if ($event->hasColumn(self::PREFIX_FOCUS.'.category')) {
             $queryBuilder->leftJoin(self::PREFIX_FOCUS, MAUTIC_TABLE_PREFIX.'categories', self::PREFIX_CATEGORIES,
                 self::PREFIX_FOCUS.'.category_id = '.self::PREFIX_CATEGORIES.'.id');
         }
+
+        switch ($event->getContext()) {
+            case self::CONTEXT_FOCUS_LEADS:
+                $queryBuilder->leftJoin(self::PREFIX_FOCUS, MAUTIC_TABLE_PREFIX.'leads', self::PREFIX_LEADS,
+                    self::PREFIX_STATS.'.lead_id = '.self::PREFIX_LEADS.'.id');
+
+                $queryBuilder->groupBy(
+                    self::PREFIX_STATS.'.focus_id',
+                    self::PREFIX_STATS.'.type',
+                    self::PREFIX_STATS.'.lead_id'
+                );
+
+                break;
+            case self::CONTEXT_FOCUS_STATS:
+                $queryBuilder->groupBy(self::PREFIX_STATS.'.focus_id', self::PREFIX_STATS.'.type');
+                break;
+        }
+
 
         $event->applyDateFilters($queryBuilder, 'date_added', self::PREFIX_STATS);
         $event->setQueryBuilder($queryBuilder);
